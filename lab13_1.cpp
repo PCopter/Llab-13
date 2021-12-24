bac6b569761e704aa77897aaf2946f078c708068
Copter <copter.phanuwat@gamil.com>
2021-12-24 14:31:52 +0700

    lab 13.1.1 ok

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
+#include <ctime>
+
+int fibonacci(int);
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+    if(x==0){return 0;}
+    else if(x==1){return 1;}
+    else{return fibonacci(x-1) + fibonacci(x-2);}
 }
